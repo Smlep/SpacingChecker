d5f08d499b3363b1c0c2aa0b69a7bbb373275f7e
Sam Courtemanche <sam.courtemanche@epita.fr>
2018-09-12 20:16:15 +0200

    Checker: Add exit status handling

--- check.sh
+++ check.sh
@@ -8,6 +8,14 @@ if [ $# -lt 2 ]; then
   echo "See $0 --help"
   exit 1
 fi
+
+code=0
+
 while IFS='' read -r line || [[ -n "$line" ]]; do
     grep -n --colour "$line" $2
+    if [ $? -ne 1 ]; then
+      code=1
+    fi
 done < "$1"
+
+exit $code

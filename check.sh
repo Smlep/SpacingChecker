c75cf47f19d7b11142c4e9241540237d92e7b10a
Sam Courtemanche <sam.courtemanche@epita.fr>
2018-09-12 19:24:29 +0200

    Checker: Added support for --help

--- check.sh
+++ check.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-
+if [ $1 == '--help' ]; then
+  cat README.md
+  exit 2
+fi
 if [ $# -lt 2 ]; then
   echo "Usage: "$0" languagefile file"
+  echo "See $0 --help"
+  exit 1
 fi
 while IFS='' read -r line || [[ -n "$line" ]]; do
     grep -n --colour "$line" $2

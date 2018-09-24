b75af5cc7de997df7065212acc6a8906be357669
Sam Courtemanche <sam.courtemanche@epita.fr>
2018-09-24 21:10:05 +0200

    Checker: refactor code

--- check.sh
+++ check.sh
@@ -14,22 +14,19 @@ fi
 
 LANGUAGE_DIR=$DIR'/languages/*'
 
-# good parameters number
-if [ $# -lt 1 ]; then
+help_opt(){
+  cat README.md
+  exit 2
+}
+
+usage_error(){
   echo "Usage: "$0" language.lg file"
   echo "See $0 --help"
   exit 1
-fi
-
-# help option
-if [ $1 == '--help' ] || [ $1 == '-h' ]; then
-  cat README.md
-  exit 2
-fi
+}
 
 # show languages option
-
-if [ "$1" == '--languages' -o "$1" == '-l' -o $1 == '--lg' ]; then
+language_help(){
   echo "Short names for available languages:"
   for lg_file in $LANGUAGE_DIR
   do
@@ -42,60 +39,88 @@ if [ "$1" == '--languages' -o "$1" == '-l' -o $1 == '--lg' ]; then
     echo $short": "$name
   done
   exit 2
-fi
+}
 
-# good parameters number
-if [ $# -lt 2 ]; then
-  echo "Usage: "$0" language.lg file"
-  echo "See $0 --help"
-  exit 1
-fi
 
-language_file='error'
-ext=$(echo "$1" | sed 's/.*\(...\)/\1/')
-echo "Looking for a language file..."
-if [ $ext == '.lg' ]; then
-  language_file=$1
-  if [ -f $1 ]; then
-    echo "Loading $1"
-  else
-    echo "$1 does not exist"
-    exit 1
-  fi
-else
-  echo "Searching files corresponding to $1"
-  for lg_file in $LANGUAGE_DIR
-  do  
-    name_line=$(head -n 1 $lg_file)
-    name="${name_line##*: }"
-
-    short_line=$(sed '2q;d' $lg_file)
-    short="${short_line##*: }"
-    
-    if [ $short == $1 ]; then
-      language_file=$lg_file
-      echo "Loading $name"
-      break
+find_language_file(){
+  language_file='error'
+  ext=$(echo "$1" | sed 's/.*\(...\)/\1/')
+  echo "Looking for a language file..."
+  if [ $ext == '.lg' ]; then
+    language_file=$1
+    if [ -f $1 ]; then
+      echo "Loading $1"
+    else
+      echo "$1 does not exist"
+      exit 1
     fi
-  done
-  if [ $language_file == 'error' ]; then
-    echo "No language file were found"
+  else
+    echo "Searching files corresponding to $1"
+    for lg_file in $LANGUAGE_DIR
+    do  
+      name_line=$(head -n 1 $lg_file)
+      name="${name_line##*: }"
+
+      short_line=$(sed '2q;d' $lg_file)
+      short="${short_line##*: }"
+    
+      if [ $short == $1 ]; then
+        language_file=$lg_file
+        echo "Loading $name"
+        break
+      fi
+    done
+    if [ $language_file == 'error' ]; then
+      echo "No language file were found"
+      exit 1
+    fi
+  fi
+
+  ext=$(echo "$language_file" | sed 's/.*\(...\)/\1/')
+  if [ $ext != '.lg' ]; then
+    echo "Bad language file extension"
+    echo "Expected: .lg"
+    echo "Has: "$ext
     exit 1
   fi
+
+  eval "$2=$language_file"
+}
+
+check_file(){
+  code=0
+
+  while IFS='' read -r line || [[ -n "$line" ]]; do
+    if [ "${line:0:1}" == '#' ] || [ "${#line}" -eq 0 ]; then
+      continue
+    fi 
+    grep -n -E --colour "$line" $2
+    if [ $? -ne 1 ]; then
+      code=1
+    fi
+  done < "$1"
+  return $code
+}
+
+
+
+# good parameter count
+if [ $# -lt 1 ]; then
+  usage_error
 fi
 
-# language file extension
-ext=$(echo "$language_file" | sed 's/.*\(...\)/\1/')
-if [ $ext != '.lg' ]; then
-  echo "Bad language file extension"
-  echo "Expected: .lg"
-  echo "Has: "$ext
-  exit 1
+# help option
+if [ $1 == '--help' ] || [ $1 == '-h' ]; then
+  help_opt
 fi
 
-if [ -z "$(tail -c 2 "$language_file")" ]; then
-  echo "Language file contains an empty line"
-  exit 1
+if [ "$1" == '--languages' -o "$1" == '-l' -o $1 == '--lg' ]; then
+  language_help
+fi
+
+# good parameter count
+if [ $# -lt 2 ]; then
+  usage_error
 fi
 
 # file to check existence
@@ -104,17 +129,9 @@ if [ ! -f $2 ]; then
   exit 1
 fi
 
-code=0
+lg_file='error'
+find_language_file $1 $lg_file
+check_file $lg_file $2
+ex_status=$?
 
-# file checking
-while IFS='' read -r line || [[ -n "$line" ]]; do
-    if [ "${line:0:1}" == '#' ] || [ "${#line}" -eq 0 ]; then
-      continue
-    fi 
-    grep -n -E --colour "$line" $2
-    if [ $? -ne 1 ]; then
-      code=1
-    fi
-done < "$language_file"
-
-exit $code
+exit $ex_status

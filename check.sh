abb74ec31e80499ae75d4be3b4267e2b8d296ab2
Sam Courtemanche <sam.courtemanche@epita.fr>
2020-03-15 10:19:15 +0100

    misc: clean check.sh to respect shellcheck

--- check.sh
+++ check.sh
@@ -5,12 +5,12 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 LANGUAGE_DIR=$DIR'/languages/*'
 
 help_opt(){
-  cat $DIR'/README.md' || cat $DIR'/../README.md'
+  cat "$DIR/README.md" || cat "$DIR/../README.md"
   exit 2
 }
 
 usage_error(){
-  echo "Usage: "$0" language.lg file"
+  echo "Usage: $0 language.lg file"
   echo "See $0 --help"
   exit 3
 }
@@ -20,13 +20,13 @@ language_help(){
   echo "Short names for available languages:"
   for lg_file in $LANGUAGE_DIR
   do
-    name_line=$(head -n 1 $lg_file)
+    name_line=$(head -n 1 "$lg_file")
     name="${name_line##*: }"
 
-    short_line=$(sed '2q;d' $lg_file)
+    short_line=$(sed '2q;d' "$lg_file")
     short="${short_line##*: }"
 
-    echo $short": "$name
+    echo "$short: $name"
   done
   exit 2
 }
@@ -36,9 +36,9 @@ find_language_file(){
   language_file='error'
   ext=$(echo "$1" | sed 's/.*\(...\)/\1/')
   echo "Looking for a language file..."
-  if [ $ext == '.lg' ]; then
+  if [ "$ext" == '.lg' ]; then
     language_file=$1
-    if [ -f $1 ]; then
+    if [ -f "$1" ]; then
       echo "Loading $1"
     else
       echo "$1 does not exist"
@@ -47,30 +47,30 @@ find_language_file(){
   else
     echo "Searching files corresponding to $1"
     for lg_file in $LANGUAGE_DIR
-    do  
-      name_line=$(head -n 1 $lg_file)
+    do
+      name_line=$(head -n 1 "$lg_file")
       name="${name_line##*: }"
 
-      short_line=$(sed '2q;d' $lg_file)
+      short_line=$(sed '2q;d' "$lg_file")
       short="${short_line##*: }"
-    
-      if [ $short == $1 ]; then
+
+      if [ "$short" == "$1" ]; then
         language_file=$lg_file
         echo "Loading $name"
         break
       fi
     done
-    if [ $language_file == 'error' ]; then
+    if [ "$language_file" == 'error' ]; then
       echo "No language file were found"
       exit 3
     fi
   fi
 
   ext=$(echo "$language_file" | sed 's/.*\(...\)/\1/')
-  if [ $ext != '.lg' ]; then
+  if [ "$ext" != '.lg' ]; then
     echo "Bad language file extension"
     echo "Expected: .lg"
-    echo "Has: "$ext
+    echo "Has: $ext"
     exit 3
   fi
 
@@ -85,9 +85,9 @@ check_file(){
   while IFS='' read -r line || [[ -n "$line" ]]; do
     if [ "${line:0:1}" == '#' ] || [ "${#line}" -eq 0 ]; then
       continue
-    fi 
-    grep -n -E --colour "$line" $2
-    count=$((count + $(grep -c -E "$line" $2)))
+    fi
+    grep -n -E --colour "$line" "$2"
+    count=$((count + $(grep -c -E "$line" "$2")))
   done < "$1"
   echo
   if [ $count -eq 0 ]; then
@@ -114,19 +114,19 @@ case $key in
     -h|--help)
     HELP=1
     shift # past argument
-    ;;  
+    ;;
     -l|--lg|--languages)
     LANGUAGE_HELP=1
     shift # past argument
-    ;;  
+    ;;
     -s|--silent)
     SILENT=1
     shift # past value
-    ;;  
+    ;;
     *)    # unknown option
     POSITIONAL+=("$1") # save it in an array for later
     shift # past argument
-    ;;  
+    ;;
 esac
 done
 set -- "${POSITIONAL[@]}" # restore positional parameters
@@ -150,17 +150,17 @@ if [ $# -lt 2 ]; then
 fi
 
 # file to check existence
-if [ ! -f $2 ]; then
+if [ ! -f "$2" ]; then
   echo "$2 does not exist"
   exit 3
 fi
 
 used_lg_file='error'
-find_language_file $1
+find_language_file "$1"
 ex_status=0
 while [[ $# -gt 1 ]]; do
-  check_file $used_lg_file $2
-  ex_status=$(($? || $ex_status))
+  check_file $used_lg_file "$2"
+  ex_status=$(($? || ex_status))
   shift
 done
 
